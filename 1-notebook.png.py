67af9034c55a71c398e11121ca39e1de818623d7
persianalien <155871677+persianalien@users.noreply.github.com>
2024-03-01 17:56:37 +0800

    Add files via upload

---
+++ 1-notebook.png.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools
+
+# In this notebook, data science tools are summarized.
+
+# 1. Python
+# 2. SQL
+# 3. R
+# 4. Java
+
+# Some of the data science libraries:
+# 
+
+# 1. TensorFlow
+# 2. NumPy
+# 3. SciPy
+# 4. Pandas
+# 5. Matplotlib
+# 6. Keras
+# 7. SciKit-Learn
+# 8. PyTorch
+# 
+
+# |Data Science Tools|
+# |---|
+# |Apache Hadoop|
+# |TensorFlow|
+# |SAS|
+
+# ### Introducing arithmetic expression examples in Python.
+
+# In[4]:
+
+
+#example of add and multiply integers
+(5*9)+4
+
+
+# In[5]:
+
+
+#this will convert 400 mins to sec
+400/60
+
+
+# ##### List of objectives 
+
+# * popular languages using fot data science 
+# * best libraries for data science 
+# * top tools for data science
+
+# ## Author Name
+# Ali Nobakhtnamini
